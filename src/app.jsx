55c0953e024195f46874f1ace099869ad579114f
Kerry <trankerry94@gmail.com>
2021-11-23 17:40:50 -0800

    half way done with bootstrap

--- src/app.jsx
+++ src/app.jsx
@@ -6,6 +6,42 @@ class App extends Component {
   }
 
   render() {
+    return (
+      <div className='container'>
+      <h1>Change Calculator</h1>
+      <hr/>
+      <div className='row'>
+        <div className='col-sm-4'>
+          <div className='card inputPanel'>
+            <div className='card-header'>Enter Information</div>
+            <div className='card-body'>
+              <form>
+                <div className='form-group'>
+                  <label htmlFor="inputDue">How much is due?</label>
+                  <input type="text" className='form-control'/>
+                </div>
+                <div className='form-group'>
+                  <label htmlFor="inputReceived">How much was received?</label>
+                  <input type="text" className='form-control'/>
+                </div>
+              </form>
+            </div>
+            <div className='card-footer'>
+              <button type="button" className="btn btn-primary">Calculate</button>
+            </div>
+          </div>
+        </div>
+        <div className='col-sm-8'>
+          <div className='outcomeAlerts'>
+            <div className="alert alert-success" role="alert">The total change due is [change amount]</div>
+            <div className="alert alert-danger" role="alert">[amount] of additional money is owed.</div>
+          </div>
+          
+        </div>
+      </div>
+
+    </div>
+    );
   }
 }
 
